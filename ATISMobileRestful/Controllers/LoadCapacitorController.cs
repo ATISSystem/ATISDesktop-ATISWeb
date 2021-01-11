67d85057feacd6d58419ec22d4169797824e0557
Biinfo878 <shsuccessbarname@gmail.com>
2021-01-11 20:16:20 +0330

    Home-139910222016

--- ATISMobileRestful/Controllers/LoadCapacitorController.cs
+++ ATISMobileRestful/Controllers/LoadCapacitorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Web.Http;
 
 using R2Core.PublicProc;
@@ -18,12 +19,13 @@ namespace ATISMobileRestful.Controllers
     public class LoadCapacitorController : ApiController
     {
         [HttpGet]
-        public List<Models.LoadCapacitorLoad> GetLoadCapacitorLoads(Int64 YourAHId, Int64 YourAHSGId, Int64 YourProvinceId = Int64.MinValue)
+        public List<Models.LoadCapacitorLoad> GetLoadCapacitorLoads(Int64 YourAHId, Int64 YourAHSGId, Int64 YourProvinceId = Int64.MinValue, Int64 YourLoadCapacitorLoadsListType = 1)
         {
             List<Models.LoadCapacitorLoad> _Loads = new List<Models.LoadCapacitorLoad>();
             try
             {
-                var Lst = R2CoreTransportationAndLoadNotificationMClassLoadCapacitorLoadManagement.GetLoadCapacitorLoads(YourAHId, YourAHSGId, Convert.ToInt64(AnnouncementHallAnnounceTimeTypes.AllOfLoadsWithoutSedimentedLoads), false, true, R2CoreTransportationAndLoadNotificationLoadCapacitorLoadOrderingOptions.TargetProvince, Int64.MinValue, YourProvinceId);
+                List<R2CoreTransportationAndLoadNotificationStandardLoadCapacitorLoadExtendedStructure> Lst =null;
+                Lst = R2CoreTransportationAndLoadNotificationMClassLoadCapacitorLoadManagement.GetLoadCapacitorLoads(YourAHId, YourAHSGId, YourLoadCapacitorLoadsListType is null ? Convert.ToInt64(AnnouncementHallAnnounceTimeTypes.AllOfLoadsWithoutSedimentedLoads) : Convert.ToInt64(YourAnnouncementHallAnnounceTimeTypes), false, true, R2CoreTransportationAndLoadNotificationLoadCapacitorLoadOrderingOptions.TargetProvince, Int64.MinValue, YourProvinceId);
                 R2CoreMClassLoggingManagement.LogRegister(new R2CoreStandardLoggingStructure(long.MinValue, R2CoreTransportationAndLoadNotificationLogType.LoadCapacitorAccessStatistics, "آمار بازدید از بار موجود در مخزن بار", YourAHId.ToString(), YourAHSGId.ToString(), String.Empty, String.Empty, String.Empty, R2Core.UserManagement.R2CoreMClassLoginManagement.GetNSSSystemUser().UserId, DateTime.Now, null));
                 for (int Loopx = 0; Loopx <= Lst.Count - 1; Loopx++)
                 {
